00590fda40e25fc2eba80861d421bd0733ea100c
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2024-03-18 16:05:33 +0100

    feat: started dog search feature

--- WorkWithVariableDataGuidedProject/WorkWithVariableDataGuidedProject/obj/Debug/net8.0/WorkWithVariableDataGuidedProject.AssemblyInfo.cs
+++ WorkWithVariableDataGuidedProject/WorkWithVariableDataGuidedProject/obj/Debug/net8.0/WorkWithVariableDataGuidedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkWithVariableDataGuidedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11a46460898ebacbded470390f38cc30a530def0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a3518c9976a1c48da4fc2da70bd61cc9b4e3b83")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkWithVariableDataGuidedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkWithVariableDataGuidedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
