9a3518c9976a1c48da4fc2da70bd61cc9b4e3b83
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2024-03-18 15:52:01 +0100

    feat: suggested donation feature added to each pets bio data

--- WorkWithVariableDataGuidedProject/WorkWithVariableDataGuidedProject/Program.cs
+++ WorkWithVariableDataGuidedProject/WorkWithVariableDataGuidedProject/Program.cs
@@ -5,14 +5,16 @@ string animalAge = "";
 string animalPhysicalDescription = "";
 string animalPersonalityDescription = "";
 string animalNickname = "";
+string suggestedDonation = "";
 
 // #2 variables that support data entry
 int maxPets = 8;
 string? readResult;
 string menuSelection = "";
+decimal decimalDonation = 0.00m;
 
 // #3 array used to store runtime data, there is no persisted data
-string[,] ourAnimals = new string[maxPets, 6];
+string[,] ourAnimals = new string[maxPets, 7];
 
 // #4 create sample data ourAnimals array entries
 for (int i = 0; i < maxPets; i++)
@@ -26,6 +28,7 @@ for (int i = 0; i < maxPets; i++)
             animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 45 pounds. housebroken.";
             animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
             animalNickname = "lola";
+            suggestedDonation = "85.00";
             break;
 
         case 1:
@@ -35,10 +38,28 @@ for (int i = 0; i < maxPets; i++)
             animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
             animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
             animalNickname = "gus";
+            suggestedDonation = "49.99";
+
             break;
 
-        // case 2: deleted for brevity
-        // case 3: deleted for brevity
+        case 2:
+            animalSpecies = "cat";
+            animalID = "C3";
+            animalAge = "1";
+            animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
+            animalPersonalityDescription = "friendly";
+            animalNickname = "snow";
+            suggestedDonation = "40.00";
+            break;
+        case 3:
+            animalSpecies = "cat";
+            animalID = "C4";
+            animalAge = "";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "lion";
+            suggestedDonation = "";
+            break;
 
         default:
             animalSpecies = "";
@@ -47,6 +68,7 @@ for (int i = 0; i < maxPets; i++)
             animalPhysicalDescription = "";
             animalPersonalityDescription = "";
             animalNickname = "";
+            suggestedDonation = "";
             break;
     }
     ourAnimals[i, 0] = "ID #: " + animalID;
@@ -55,6 +77,14 @@ for (int i = 0; i < maxPets; i++)
     ourAnimals[i, 3] = "Nickname: " + animalNickname;
     ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
     ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
+    ourAnimals[i, 6] = "Suggested Donation: "+suggestedDonation;
+
+    if(!decimal.TryParse(suggestedDonation, out decimalDonation))
+    {
+        decimalDonation = 45.00m; // if suggestedDonation NOT a number, default to 45.00
+    }
+
+    ourAnimals[i, 6] = $"Suggested Donation: {decimalDonation:C2}";
 }
 // #5 display the top-level menu options
 do
@@ -84,7 +114,7 @@ do
                 if (ourAnimals[i, 0] != "ID #: ")
                 {
                     Console.WriteLine();
-                    for (int j = 0; j < 6; j++)
+                    for (int j = 0; j < 7; j++)
                     {
                         Console.WriteLine(ourAnimals[i, j]);
                     }
--- WorkWithVariableDataGuidedProject/WorkWithVariableDataGuidedProject/obj/Debug/net8.0/WorkWithVariableDataGuidedProject.AssemblyInfo.cs
+++ WorkWithVariableDataGuidedProject/WorkWithVariableDataGuidedProject/obj/Debug/net8.0/WorkWithVariableDataGuidedProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkWithVariableDataGuidedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5244bb27984aee7040ea19e705ed5a2843431005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11a46460898ebacbded470390f38cc30a530def0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkWithVariableDataGuidedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkWithVariableDataGuidedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
